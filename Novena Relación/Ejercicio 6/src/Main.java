d3d08a4a42a3b02493ba119708883f8fe4d65ee8
JuanGassinDaw <juangaming97@gmail.com>
2023-01-18 12:58:47 +0100

    ole

--- Novena Relación/Ejercicio 6/src/Main.java
+++ Novena Relación/Ejercicio 6/src/Main.java
@@ -1,6 +1,11 @@
 public class Main {
     public static void main(String[] args) {
 
-
+        Tiempo t1 = new Tiempo(1, 50, 310);
+        Tiempo t2 = new Tiempo(0, 30, 140);
+        Tiempo t3 = new Tiempo(0, 35, 20);
+        System.out.println(t1);
+        System.out.println(t1 + " + " + t2 + " = " + t1.suma(t2));
+        System.out.println(t1 + " - " + t3 + " = " + t3.resta(t1));
     }
 }
--- Novena Relación/Ejercicio 6/src/Tiempo.java
+++ Novena Relación/Ejercicio 6/src/Tiempo.java
@@ -1,29 +1,37 @@
 public class Tiempo {
 
     //ATRIBUTOS
-    int horas;
-    int minutos;
     int segundos;
 
     //CONSTRUCTOR
     public Tiempo(int horas, int minutos, int segundos) {
         this.segundos = (horas * 3600) + (minutos * 60) + segundos;
     }
-
+    public Tiempo(int minutos,int segundos){
+        this.segundos = (minutos * 60) + segundos;
+    }
     public Tiempo(int segundos){
         this.segundos = segundos;
     }
+
     //METODOS
-
-
     public int getSegundos() {
         return segundos;
     }
 
 
-    public Tiempo suma(Tiempo t2){
+    public Tiempo suma(Tiempo t){
 
-        t2.segundos += getSegundos();
+        int segundos = t.segundos + getSegundos();
+        Tiempo t2 = new Tiempo(segundos);
+
+        return t2;
+    }
+
+    public Tiempo resta(Tiempo t){
+
+        int segundos = t.segundos - getSegundos();
+        Tiempo t2 = new Tiempo(segundos);
 
         return t2;
     }
@@ -31,6 +39,14 @@ public class Tiempo {
 
     @Override
     public String toString() {
+
+        int horas, minutos, segundos;
+        segundos = this.segundos;
+        horas = (segundos / 3600);
+        minutos = ((segundos - horas * 3600) / 60);
+        segundos = segundos - (horas * 3600 + minutos * 60);
+
+
         return "Tiempo = " + horas + "h " + minutos + "m " + segundos + "s";
     }
 }
