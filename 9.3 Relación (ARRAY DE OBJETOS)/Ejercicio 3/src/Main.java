fc34287997418a8c4cb4872768b9b2c42a6605f1
JuanGassinDaw <juangaming97@gmail.com>
2023-02-01 10:56:35 +0100

    Ole y ole

--- 9.3 Relación (ARRAY DE OBJETOS)/Ejercicio 3/src/Main.java
+++ 9.3 Relación (ARRAY DE OBJETOS)/Ejercicio 3/src/Main.java
@@ -4,7 +4,10 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
-        int o = 0,n = 100;
+        int o = 0,n = 100,cont = 0,cont2 = 0;
+
+        String codigo,autor,titulo,genero;
+        int duracion;
 
         Disco colecciondisco[]= new Disco[n];
 
@@ -20,22 +23,100 @@ public class Main {
             System.out.println("\uD83D\uDCBFMENÚ COLECCIÓN-DISCOS\uD83D\uDCBF");
             System.out.println("-------------------------------");
             System.out.println("1.Listado");
-            System.out.println("2.Modificar");
-            System.out.println("3.Borrar");
-            System.out.println("4.Salir");
+            System.out.println("2.Nuevo");
+            System.out.println("3.Modificar");
+            System.out.println("4.Borrar");
+            System.out.println("5.Salir");
+
             int opcion;
+            System.out.print("Dime que opcion quieres:");
             opcion = reader.nextInt();
 
             switch (opcion){
                 case 1:{
-                    for (int i = 0; i < colecciondisco.length;i++){
-                        if(dis)
-                        System.out.println(colecciondisco[i]);
+                    for(Disco d : colecciondisco) {
+                        if (!d.getCodigo().equals("LIBRE")) {
+                            System.out.println(d);
+                        }
                     }
                 }break;
 
                 case 2:{
 
+                    for (Disco d:colecciondisco){
+                        if(!d.getCodigo().equals("LIBRE")){
+                            cont2++;
+                        }
+                    }
+                    System.out.println(o);
+                    if(cont2 >= 99){
+                        System.out.println(o);
+                    }
+                    System.out.println("Creando nuevo disco");
+                    System.out.print("Titulo:");
+                    titulo = reader.next();
+                    System.out.print("Autor:");
+                    autor = reader.next();
+                    System.out.print("Género:");
+                    genero = reader.next();
+                    System.out.print("Código:");
+                    codigo = reader.next();
+                    System.out.print("Duración:");
+                    duracion = reader.nextInt();
+
+                    for(Disco d : colecciondisco) {
+                        if (!d.getCodigo().equals("LIBRE")) {
+                            cont++;
+                        }
+                    }
+                    colecciondisco[cont] = new Disco(codigo,autor,titulo,genero,duracion);
+                    System.out.println("!Nuevo disco añadido¡");
+                }break;
+
+                case 3:{
+
+                    System.out.print("Dime el código del disco que quieres:");
+                    codigo = reader.next();
+
+                    for (Disco d:colecciondisco){
+                        if(d.getCodigo().equals(codigo)){
+
+                            System.out.println("Modificando disco seleccionado");
+                            System.out.print("Nuevo Titulo:");
+                            titulo = reader.next();
+                            System.out.print("Nuevo Autor:");
+                            autor = reader.next();
+                            System.out.print("Nuevo Género:");
+                            genero = reader.next();
+                            System.out.print("Nuevo Código:");
+                            codigo = reader.next();
+                            System.out.print("Nuevo Duración:");
+                            duracion = reader.nextInt();
+
+                            d.setCodigo(codigo);
+                            d.setAutor(autor);
+                            d.setDuracion(duracion);
+                            d.setGenero(genero);
+                            d.setTitulo(titulo);
+
+                            System.out.println("Disco Modificado con éxito");
+                        }
+                    }
+                }break;
+
+                case 4:{
+                    System.out.print("Dime el código del disco que quieres:");
+                    codigo = reader.next();
+
+                    for (Disco d:colecciondisco){
+                        if(d.getCodigo().equals(codigo)){
+                            d.setCodigo("LIBRE");
+                        }
+                    }
+                }break;
+
+                case 5:{
+                    o++;
                 }break;
             }
         }
