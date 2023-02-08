2eb1e790d859c42db442fa8a83dfbb526482611f
JuanGassinDAW <juangaming97@gmail.com>
2023-02-08 02:27:05 +0100

    cositis

--- 10 Relación/Ejercicio 8/src/Cartas.java
+++ 10 Relación/Ejercicio 8/src/Cartas.java
@@ -1,9 +1,37 @@
+import java.util.Objects;
+
 public class Cartas {
 
-    public enum Palo{
-        Bastos,Copas,Oros,Espadas
+    //ATRIBUTOS
+    String[] numero = {"as","dos","tres","cuatro","cinco","seis","siete","sota","caballo","rey"};
+    String[] palo = {"Bastos","Copas","Oros","Espadas"};
+    String n;
+    String p;
+
+    //CONSTRUCTORES
+    public Cartas() {
+        this.n = numero[(int)(Math.random()*10)];
+        this.p = palo[(int)(Math.random()*4)];
+
     }
 
-    //ATRIBUTOS
-    String
+    //METODOS
+
+    public String getN() {
+        return n;
+    }
+
+    public String getP() {
+        return p;
+    }
+
+    //TOSTRING
+
+
+    @Override
+    public String toString() {
+        return n + " de " + p;
+    }
+
+
 }
--- 10 Relación/Ejercicio 8/src/Main.java
+++ 10 Relación/Ejercicio 8/src/Main.java
@@ -1,5 +1,24 @@
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
 
+        ArrayList<Cartas> Baraja = new ArrayList<>();
+
+        Cartas a = new Cartas();
+        Baraja.add(a);
+
+        for(int i = 0; i < 9;i++){
+
+            while(Baraja.contains(a)){
+                a = new Cartas();
+            }
+
+            Baraja.add(a);
+        }
+
+        for(Cartas c:Baraja){
+            System.out.println(c);
+        }
     }
 }
