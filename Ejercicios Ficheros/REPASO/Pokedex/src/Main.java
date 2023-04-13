547830d251bc2527ac190cb7d25ca5ed1befe22c
JuanGassinDaw <juangaming97@gmail.com>
2023-04-13 12:30:25 +0200

    oleiola

---
+++ Ejercicios Ficheros/REPASO/Pokedex/src/Main.java
@@ -0,0 +1,35 @@
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
+
+            Pokedex pq = new Pokedex();
+
+            int opcion = -1;
+
+            while(opcion != 0){
+
+                System.out.println("Menú de la Pokedex");
+                System.out.println("---------------------------");
+                System.out.println("1) Captura Pokemon");
+                System.out.println("2) Porcentaje Capturas");
+                System.out.println("3) Busca Pokemon por ID");
+                System.out.println("4) Busca Pokemon por Nombre");
+                System.out.println("5) Listado Pokemon");
+
+                System.out.print("Elije tu ópcion:");
+                opcion = reader.nextInt();
+
+                switch (opcion){
+                    case 1:{
+                        int id;
+                        id = reader.nextInt();
+                        pq.buscaPokemon(id);
+                    }break;
+                }
+            }
+
+    }
+}
---
+++ Ejercicios Ficheros/REPASO/Pokedex/src/Pokedex.java
@@ -0,0 +1,131 @@
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.Scanner;
+
+public class Pokedex {
+
+    Scanner reader = new Scanner(System.in);
+    //ATRIBUTOS
+    ArrayList<Pokemon> listaPokemon;
+
+    //CONSTRUCTOR
+
+    public Pokedex() throws FileNotFoundException {
+        listaPokemon = new ArrayList<>();
+        leeCSV("ficheroPokemons.csv");
+    }
+
+    //METODOS
+    public void leeCSV(String nombreFichero) throws FileNotFoundException{
+
+        try{
+            BufferedReader br = new BufferedReader(new FileReader(nombreFichero));
+
+            String line = br.readLine();
+            while(null != line){
+                String[] p = line.split(",");
+
+                int id = Integer.parseInt(p[0]);
+                String nombre = p[1];
+                String tipo1 = p[2];
+                String tipo2 = p[3];
+                int vida = Integer.parseInt(p[4]);
+                int ataque = Integer.parseInt(p[5]);
+                int ataqueEspecial = Integer.parseInt(p[6]);
+                int defensa = Integer.parseInt(p[7]);
+                int defensaEspecial = Integer.parseInt(p[8]);
+                int velocidad = Integer.parseInt(p[9]);
+                boolean capturado;
+
+                if(p[10] == "false"){
+                    capturado = false;
+                }else{
+                    capturado = true;
+                }
+
+                listaPokemon.add(new Pokemon(id,nombre,tipo1,tipo2,vida,ataque,ataqueEspecial,defensa,defensaEspecial,velocidad,capturado));
+            }
+
+            br.close();
+
+        } catch (IOException e) {
+            System.out.println("error!!" + e.getMessage());
+        }
+    }
+
+    public void guardaCSV(String nombreFichero) throws FileNotFoundException {
+        try {
+            BufferedWriter bw = new BufferedWriter(new FileWriter(nombreFichero));
+        } catch (IOException e) {
+            System.out.println("error!!" + e.getMessage());
+        }
+
+    }
+
+    public void capturaPokemon(){
+        String nombrePokemon;
+
+        System.out.println("Dime el nombre del personaje que has capturado");
+        nombrePokemon = reader.next();
+
+        for(Pokemon p: listaPokemon){
+            if(Objects.equals(p.nombre, nombrePokemon)){
+                p.capturado = true;
+            }
+        }
+    }
+
+    public double porcentajeCapturas(){
+        int totalCapturados = 0,nPokemon = 0;
+        double porcentaje;
+        for(Pokemon p: listaPokemon){
+            nPokemon++;
+            if(p.capturado = true){
+                totalCapturados++;
+            }
+        }
+
+        porcentaje = ((double)(totalCapturados/nPokemon))*100;
+
+        return porcentaje;
+    }
+
+    public Pokemon buscaPokemon(int id) {
+
+        Pokemon poke = null;
+
+        for (Pokemon p : listaPokemon) {
+            if (id == p.id) {
+                poke = p;
+            }
+        }
+        return poke;
+    }
+
+    public Pokemon buscaPokemon(String nombrePoke) {
+
+        Pokemon poke = null;
+
+        for (Pokemon p : listaPokemon) {
+            if (Objects.equals(nombrePoke, p.nombre)) {
+                poke = p;
+            }
+        }
+        return poke;
+    }
+
+    public void listadoPokemon(){
+
+        String [] listPoke;
+        int i = 0;
+
+        for(Pokemon p : listaPokemon){
+
+        }
+    }
+
+
+
+}
